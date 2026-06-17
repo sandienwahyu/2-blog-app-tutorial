ad8c3260faef7fe355e8dba081841bcc1f9f6450
sandienwahyu <sandienwahyu@gmail.com>
2026-06-17 16:55:57 +0700

    making portfolio menu

--- app/portfolio/app/page.tsx
+++ app/portfolio/app/page.tsx
@@ -1,10 +1,34 @@
-"use client";
-export default function AppPage() {
+import Image from "next/image";
+
+export default async function AppPage() {
+  const data = await fetch("https://fakestoreapi.com/products");
+  const res = await data.json();
+
   return (
     <>
-      <h4 className="text-lg font-semibold text-primary/70 ml-1">App</h4>
-      <div className="flex">
-        <div className="flex">Content</div>
+      <h4 className="text-3xl font-semibold text-primary/70 ml-1 mb-4">App</h4>
+      <div className="flex flex-col gap-6">
+        {res.map((item: any) => (
+          <div
+            key={item.id}
+            className={`flex ${item.id % 2 === 0 ? "flex-row" : "flex-row-reverse"} gap-5 min-h-52 justify-center`}
+          >
+            <div className="w-1/2 flex flex-col gap-2 justify-center">
+              <h6 className="text-lg font-semibold">{item.title}</h6>
+              <p className="text-sm text-justify text-foreground/80">
+                {item.description}
+              </p>
+            </div>
+            <div className="w-1/2 overflow-hidden relative">
+              <Image
+                src={item.image}
+                alt={item.title}
+                fill
+                className="object-contain rounded"
+              />
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
--- app/portfolio/layout.tsx
+++ app/portfolio/layout.tsx
@@ -1,7 +1,7 @@
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col flex-1 justify-center gap-4 overflow-hidden">
-      <h1 className="text-4xl font-bold">Our Works</h1>
+      <h1 className="text-6xl font-bold">Our Works</h1>
       {children}
     </div>
   );

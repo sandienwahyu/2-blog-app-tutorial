078bc5ab31fb25dae58563d41d85fe362bc6b594
sandienwahyu <sandienwahyu@gmail.com>
2026-06-18 16:20:33 +0700

    making blog menu

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
 interface BlogPostPageProps {
   params: Promise<{
     id: string; // Must exactly match the directory name [id]
@@ -6,5 +9,59 @@ interface BlogPostPageProps {
 
 export default async function BlogPostPage({ params }: BlogPostPageProps) {
   const { id } = await params;
-  return <div>BlogPost: {id}</div>;
+  const data = await fetch(`https://api.vercel.app/blog/${id}`);
+  const blog = await data.json();
+
+  if (!data.ok) {
+    return notFound();
+  }
+
+  return (
+    <div className="flex-1 flex flex-col gap-6 mt-4">
+      <div className="flex gap-4 items-center justify-between">
+        <div className="flex-1 justify-center flex flex-col gap-1">
+          <h1 className="text-3xl font-bold">{blog.title}</h1>
+          <p className="text-lg text-foreground">{blog.content}</p>
+          <div className="flex-1 flex gap-2 justify-start items-center">
+            <div className="rounded-full w-8 h-8 bg-gray-300"></div>
+            <span className="text-sm text-foreground/80">{blog.author}</span>
+          </div>
+        </div>
+        <div className="flex-1 h-44 relative overflow-hidden">
+          <Image
+            src="/app-portfolio.jpg"
+            alt={blog.title}
+            fill
+            className="object-cover"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col gap-4 text-foreground/80 text-justify">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam eum
+          fuga repellendus quasi magni beatae suscipit, reiciendis libero? Ut
+          architecto in expedita obcaecati enim quae praesentium odit molestiae
+          vero qui. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Aperiam eum fuga repellendus quasi magni beatae suscipit, reiciendis
+          libero? Ut architecto in expedita obcaecati enim quae praesentium odit
+          molestiae vero qui.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam eum
+          fuga repellendus quasi magni beatae suscipit, reiciendis libero? Ut
+          architecto in expedita obcaecati enim quae praesentium odit molestiae
+          vero qui.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam eum
+          fuga repellendus quasi magni beatae suscipit, reiciendis libero? Ut
+          architecto in expedita obcaecati enim quae praesentium odit molestiae
+          vero qui. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Aperiam eum fuga repellendus quasi magni beatae suscipit, reiciendis
+          libero? Ut architecto in expedita obcaecati enim quae praesentium odit
+          molestiae vero qui.
+        </p>
+      </div>
+    </div>
+  );
 }
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,4 +1,45 @@
-"use client";
-export default function Blog() {
-  return <div>Blog</div>;
+import Image from "next/image";
+import Link from "next/link";
+
+export default async function Blog() {
+  const data = await fetch("https://api.vercel.app/blog");
+  const res = await data.json();
+
+  return (
+    <div className="flex flex-1 flex-col gap-6 mt-4">
+      <div>
+        <h1 className="text-5xl font-bold text-primary/80">Blog List</h1>
+      </div>
+      <div className="flex flex-col gap-2">
+        {res.map((blog: any) => (
+          <Link
+            key={blog.id}
+            href={`/blog/${blog.id}`}
+            className="flex justify-between rounded-lg bg-secondary/75 hover:bg-secondary p-4"
+          >
+            <div className="flex flex-col gap-1 justify-center">
+              <h2 className="text-xl font-semibold">
+                {blog.title.length > 30
+                  ? blog.title.substring(0, 30) + "..."
+                  : blog.title}
+              </h2>
+              <p className="text-sm text-foreground/80">
+                {blog.content.length > 200
+                  ? blog.content.substring(0, 200) + "..."
+                  : blog.content}
+              </p>
+            </div>
+            <div className="relative w-1/3 min-h-24 rounded-lg overflow-hidden">
+              <Image
+                src="/website-portfolio.jpg"
+                alt={blog.title}
+                fill
+                className="object-cover scale-115 translate-x-4 object-[50%_20%]"
+              />
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }

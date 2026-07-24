9f455dbd235e21e9da552c9127e9df38ca171c3b
sandienwahyu <sandienwahyu@gmail.com>
2026-07-24 12:51:07 +0700

    adding baseURL on betterAuth

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,6 +3,7 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./db";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
   secret: process.env.BETTER_AUTH_SECRET!,
   database: prismaAdapter(prisma, {
     provider: "postgresql",

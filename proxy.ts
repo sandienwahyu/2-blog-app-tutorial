be8f19b3fddccd0cae7ea302d1353db0962c2ec8
sandienwahyu <sandienwahyu@gmail.com>
2026-07-24 13:36:16 +0700

    debugging the proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -5,6 +5,14 @@ const publicPages = ["/login", "/register"];
 export function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
 
+  console.log("=== MIDDLEWARE DEBUG ===");
+  console.log("Pathname:", pathname);
+  console.log("All cookies:", request.cookies.getAll());
+  console.log(
+    "Token:",
+    request.cookies.get("better-auth.session_token")?.value,
+  );
+
   // Allow public pages
   if (publicPages.includes(pathname)) {
     return NextResponse.next();
